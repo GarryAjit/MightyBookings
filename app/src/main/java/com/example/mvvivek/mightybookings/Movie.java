8d402082d631147cb37f6b83c824333986ee2a8f
vivek <vivekmv_9@yahoo.in>
2018-02-22 18:28:20 +0530

    Added app

---
+++ app/src/main/java/com/example/mvvivek/mightybookings/Movie.java
@@ -0,0 +1,12 @@
+package com.example.mvvivek.mightybookings;
+
+/**
+ * Created by mvvivek on 22/02/2018.
+ */
+
+public class Movie
+{
+    private String hall1;
+    private String hall2;
+    
+}
